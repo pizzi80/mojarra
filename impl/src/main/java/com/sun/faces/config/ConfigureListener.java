18c75b4f902f3f88988759b686e9d054aa7ad090
pizzi80 <paolo@given2.com>
2024-10-18 19:05:49 +0200

    ConfigureListener: version 5.0.22
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/config/ConfigureListener.java
+++ impl/src/main/java/com/sun/faces/config/ConfigureListener.java
@@ -170,7 +170,7 @@ public class ConfigureListener implements ServletRequestListener, HttpSessionLis
 
         try {
             if (LOGGER.isLoggable(INFO)) {
-                LOGGER.log(INFO, MessageFormat.format("Initializing Mojarra {0} for context {1}", "5.0.20_pizzi" , servletContext.getContextPath()) );
+                LOGGER.log(INFO, MessageFormat.format("Initializing Mojarra {0} for context {1}", "5.0.22_pizzi" , servletContext.getContextPath()) );
             }
 
             if (webConfig.isOptionEnabled(VerifyFacesConfigObjects)) {
