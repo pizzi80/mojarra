49fec320e759587933b76dd022398bc23d81d698
pizzi80 <paolo@given2.com>
2025-02-07 13:58:15 +0100

    FilterClassesFromFacesInitializerAnnotationProvider: avoid useless HashMap creation + proper set HashMap size + lambda expressions + cosmetics + for loop
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/config/manager/spi/FilterClassesFromFacesInitializerAnnotationProvider.java
+++ impl/src/main/java/com/sun/faces/config/manager/spi/FilterClassesFromFacesInitializerAnnotationProvider.java
@@ -24,14 +24,17 @@ import static java.util.Arrays.stream;
 
 import java.lang.annotation.Annotation;
 import java.net.URI;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import com.sun.faces.config.WebConfiguration;
 import com.sun.faces.spi.AnnotationProvider;
+import com.sun.faces.util.Util;
 
 import jakarta.servlet.ServletContext;
 
@@ -46,6 +49,8 @@ import jakarta.servlet.ServletContext;
  *
  */
 public class FilterClassesFromFacesInitializerAnnotationProvider extends AnnotationProvider {
+
+    private static final Pattern WHITESPACES = Pattern.compile("\\s+");
 
     public FilterClassesFromFacesInitializerAnnotationProvider(ServletContext servletContext) {
         super(servletContext);
@@ -71,28 +76,28 @@ public class FilterClassesFromFacesInitializerAnnotationProvider extends Annotat
     /**
      * Go over the annotated set and converter it to a hash map.
      *
-     * @param annotatedMap
      * @param annotatedSet
      */
-    private Map<Class<? extends Annotation>, Set<Class<?>>> createAnnotatedMap(Set<Class<?>> annotatedSet) {
-        HashMap<Class<? extends Annotation>, Set<Class<?>>> annotatedMap = new HashMap<>();
+    private static Map<Class<? extends Annotation>, Set<Class<?>>> createAnnotatedMap(Set<Class<?>> annotatedSet) {
 
         if (isEmpty(annotatedSet)) {
-            return annotatedMap;
+            return Collections.emptyMap();
         }
 
-        WebConfiguration webConfig = WebConfiguration.getInstance();
-        boolean annotationScanPackagesSet = webConfig.isSet(AnnotationScanPackages);
-
-        String[] annotationScanPackages = annotationScanPackagesSet ? webConfig.getOptionValue(AnnotationScanPackages).split("\\s+") : null;
+        final Map<Class<? extends Annotation>, Set<Class<?>>> annotatedMap = new HashMap<>(Util.calculateMapCapacity(annotatedSet.size()));
+        final WebConfiguration webConfig = WebConfiguration.getInstance();
+        final boolean annotationScanPackagesSet = webConfig.isSet(AnnotationScanPackages);
+        final String[] annotationScanPackages = annotationScanPackagesSet ? WHITESPACES.split(webConfig.getOptionValue(AnnotationScanPackages)) : null;
 
         Iterator<Class<?>> iterator = annotatedSet.iterator();
         while (iterator.hasNext()) {
             try {
                 Class<?> clazz = iterator.next();
 
-                stream(clazz.getAnnotations()).map(annotation -> annotation.annotationType())
-                        .filter(annotationType -> FACES_ANNOTATION_TYPE.contains(annotationType)).forEach(annotationType -> {
+                stream(clazz.getAnnotations())
+                        .map(Annotation::annotationType)
+                        .filter(FACES_ANNOTATION_TYPE::contains)
+                        .forEach(annotationType -> {
 
                             Set<Class<?>> classes = annotatedMap.computeIfAbsent(annotationType, e -> new HashSet<>());
 
@@ -106,28 +111,28 @@ public class FilterClassesFromFacesInitializerAnnotationProvider extends Annotat
 
                         });
 
-            } catch (NoClassDefFoundError ncdfe) {
             }
+            catch (NoClassDefFoundError ignored) {}
         }
 
         return annotatedMap;
     }
 
-    private boolean matchesAnnotationScanPackages(Class<?> clazz, String[] annotationScanPackages) {
+    private static boolean matchesAnnotationScanPackages(Class<?> clazz, String[] annotationScanPackages) {
         boolean result = false;
 
-        for (int i = 0; i < annotationScanPackages.length; i++) {
+        for (String annotationScanPackage : annotationScanPackages) {
 
             String classUrlString = clazz.getProtectionDomain().getCodeSource().getLocation().toString();
             String classPackageName = clazz.getPackage().getName();
 
-            if (classUrlString.contains("WEB-INF/classes") && annotationScanPackages[i].equals("*")) {
+            if (classUrlString.contains("WEB-INF/classes") && annotationScanPackage.equals("*")) {
                 result = true;
-            } else if (classPackageName.equals(annotationScanPackages[i])) {
+            } else if (classPackageName.equals(annotationScanPackage)) {
                 result = true;
-            } else if (annotationScanPackages[i].startsWith("jar:")) {
-                String jarName = annotationScanPackages[i].substring(4, annotationScanPackages[i].indexOf(":", 5));
-                String jarPackageName = annotationScanPackages[i].substring(annotationScanPackages[i].lastIndexOf(":") + 1);
+            } else if (annotationScanPackage.startsWith("jar:")) {
+                String jarName = annotationScanPackage.substring(4, annotationScanPackage.indexOf(':', 5));
+                String jarPackageName = annotationScanPackage.substring(annotationScanPackage.lastIndexOf(':') + 1);
                 if (jarName.equals("*")) {
                     if (jarPackageName.equals("*")) {
                         result = true;
