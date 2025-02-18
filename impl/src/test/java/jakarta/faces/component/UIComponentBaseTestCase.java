169c0fd2af610f5522f33bc2c1496c605ccdd90a
pizzi80 <paolo@given2.com>
2025-02-18 17:27:04 +0100

    UIComponentBaseTestCase: use RenderKitUtils constants
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/test/java/jakarta/faces/component/UIComponentBaseTestCase.java
+++ impl/src/test/java/jakarta/faces/component/UIComponentBaseTestCase.java
@@ -30,7 +30,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
+import com.sun.faces.renderkit.RenderKitUtils;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -369,14 +370,14 @@ public class UIComponentBaseTestCase extends UIComponentTestCase {
         c.getAttributes().put("attr1", "value1");
         c.markInitialState();
         c.getAttributes().put("attr2", "value2");
-        assertEquals(Arrays.asList("attr1", "attr2"), c.getAttributes().get("jakarta.faces.component.UIComponentBase.attributesThatAreSet"));
+        assertEquals(Arrays.asList("attr1", "attr2"), c.getAttributes().get(RenderKitUtils.ATTRIBUTES_THAT_ARE_SET_KEY));
 
         Object state = c.saveState(facesContext);
         c = new ComponentTestImpl();
         c.pushComponentToEL(facesContext, c);
         c.restoreState(facesContext, state);
         c.popComponentFromEL(facesContext);
-        assertEquals(Arrays.asList("attr2"), c.getAttributes().get("jakarta.faces.component.UIComponentBase.attributesThatAreSet"));
+        assertEquals(Arrays.asList("attr2"), c.getAttributes().get(RenderKitUtils.ATTRIBUTES_THAT_ARE_SET_KEY));
     }
 
     @Test
