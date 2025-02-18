ab80cedb7851f7383a475e2e052d6c03c34dbbf9
pizzi80 <paolo@given2.com>
2025-02-18 11:42:15 +0100

    ResponseStreamWrapper implementation
    Signed-off-by: pizzi80 <paolo@given2.com>

---
+++ impl/src/main/java/jakarta/faces/context/ResponseStreamWrapper.java
@@ -0,0 +1,39 @@
+package jakarta.faces.context;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ResponseStreamWrapper extends ResponseStream {
+
+    private final OutputStream os;
+
+    public ResponseStreamWrapper(OutputStream os) {
+        this.os = os;
+    }
+
+    @Override
+    public void close() throws IOException {
+        os.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        os.flush();
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        os.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        os.write(b, off, len);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        os.write(b);
+    }
+
+}
