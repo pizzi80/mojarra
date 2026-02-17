5804c184e2495e81e9e0b0740ccbb0edd7b91f38
pizzi80 <paolo@given2.com>
2026-02-17 14:44:03 +0100

    ELFlash equals cosmetics
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/context/flash/ELFlash.java
+++ impl/src/main/java/com/sun/faces/context/flash/ELFlash.java
@@ -1433,11 +1433,10 @@ public class ELFlash extends Flash {
 
         @Override
         public boolean equals(Object obj) {
-            if ( !(obj instanceof FlashInfo info) ) return false;
-
-            return  isRedirect == info.isRedirect &&
-                    Objects.equals(lifetimeMarker, info.lifetimeMarker) &&
-                    sequenceNumber == info.sequenceNumber;
+            return obj instanceof FlashInfo info
+                && isRedirect == info.isRedirect
+                && Objects.equals(lifetimeMarker, info.lifetimeMarker)
+                && sequenceNumber == info.sequenceNumber;
         }
 
         @Override
