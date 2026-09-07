b5c70a621b54be6ad141f81ab3e691c940a7d6e6
pizzi80 <paolo@given2.com>
2026-09-07 20:01:41 +0200

    4.2.16.1 pizzi release
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/config/ConfigureListener.java
+++ impl/src/main/java/com/sun/faces/config/ConfigureListener.java
@@ -182,7 +182,7 @@ public class ConfigureListener implements ServletRequestListener, HttpSessionLis
         try {
 
             if (LOGGER.isLoggable(INFO)) {
-                LOGGER.log(INFO, "Initializing Mojarra %s for context %s".formatted("4.2.16_pizzi", servletContext.getContextPath()) );
+                LOGGER.log(INFO, "Initializing Mojarra %s for context %s".formatted("4.2.16.1_pizzi", servletContext.getContextPath()) );
             }
 
             if (webConfig.isOptionEnabled(VerifyFacesConfigObjects)) {
