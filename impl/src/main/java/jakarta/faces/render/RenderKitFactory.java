a3c9e8ce6b2edb3a221de7b58cbb1a3669c700ab
pizzi80 <paolo@given2.com>
2026-09-23 16:43:12 +0200

    RenderKitFactory reduce diff with upstream
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/jakarta/faces/render/RenderKitFactory.java
+++ impl/src/main/java/jakarta/faces/render/RenderKitFactory.java
@@ -52,7 +52,7 @@ public abstract class RenderKitFactory implements FacesWrapper<RenderKitFactory>
     /**
      * @deprecated Use the other constructor taking the implementation being wrapped.
      */
-    @Deprecated(forRemoval=true)
+    @Deprecated
     public RenderKitFactory() {
         wrapped = null;
     }
