bb5b6571de1f46a782540b9b6fe177925518f1ea
pizzi80 <paolo@given2.com>
2026-09-23 14:18:43 +0200

    CdiConverter reduce diff with upstream
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/com/sun/faces/cdi/CdiConverter.java
+++ impl/src/main/java/com/sun/faces/cdi/CdiConverter.java
@@ -141,8 +141,11 @@ public class CdiConverter implements Converter, StateHolder {
      */
     private Converter getDelegate(FacesContext facesContext) {
         if (delegate == null) {
-            // if the converterId is not defined, use the forClass property
-            delegate = converterId.isEmpty() ? facesContext.getApplication().createConverter(forClass) : facesContext.getApplication().createConverter(converterId);
+            if (!converterId.isEmpty()) {
+                delegate = facesContext.getApplication().createConverter(converterId);
+            } else {
+                delegate = facesContext.getApplication().createConverter(forClass);
+            }
         }
         return delegate;
     }
