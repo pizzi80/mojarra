9044177261309926d9b8f2a5e47917e5c02ecbc0
pizzi80 <paolo@given2.com>
2026-09-24 14:21:03 +0200

    ServletContextFacesContextFactory reduce diff with upstream
    Signed-off-by: pizzi80 <paolo@given2.com>

--- impl/src/main/java/jakarta/faces/ServletContextFacesContextFactory.java
+++ impl/src/main/java/jakarta/faces/ServletContextFacesContextFactory.java
@@ -56,7 +56,6 @@ final class ServletContextFacesContextFactory extends FacesContextFactory {
 
     @SuppressWarnings("unchecked")
     ServletContextFacesContextFactory() {
-        super(null);
         try {
             Field instanceField = FacesContext.class.getDeclaredField("instance");
             instanceField.setAccessible(true);
